88758d03c3571240fd310673c23d0689619a6e7b
Marko Parfeniuk <90197701+markoparfeniuk@users.noreply.github.com>
2023-05-18 13:49:35 +0300

    update main.py

--- L4/SalesmanProblem/main.py
+++ L4/SalesmanProblem/main.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 # N = random.randint(25, 35)
 # cities = range(N)
@@ -34,18 +35,18 @@ Q = 100
 pheromones = [[1 / (N * N) for _ in cities] for _ in cities]
 
 def length(tour):
-    """Calculate the length of a tour"""
+    # Calculate the length of a tour
     return sum(distances[tour[i - 1]][tour[i]] for i in cities)
 
 def transition_probability(alpha, beta, visited):
-    """Calculate the transition probabilities for ant k"""
+    # Calculate the transition probabilities for ant k
     unvisited = set(cities) - set(visited)
     denominator = sum(pheromones[visited[-1]][j] ** alpha * (1 / distances[visited[-1]][j]) ** beta for j in unvisited)
     probabilities = [pheromones[visited[-1]][j] ** alpha * (1 / distances[visited[-1]][j]) ** beta / denominator if j in unvisited else 0 for j in cities]
     return probabilities
 
 def construct_solution(M, N, alpha, beta):
-    """Construct a solution using the ant algorithm"""
+    # Construct a solution using the ant algorithm
     tours = [[] for _ in range(M)]
     for k in range(M):
         tours[k].append(random.randint(0, N - 1))
@@ -57,7 +58,7 @@ def construct_solution(M, N, alpha, beta):
     return tours
 
 def update_pheromones(tours, M, N, rho):
-    """Update pheromone levels"""
+    # Update pheromone levels
     L = [length(tour) for tour in tours]
     delta_pheromones = [[0 for _ in cities] for _ in cities]
     for k in range(M):
@@ -67,71 +68,48 @@ def update_pheromones(tours, M, N, rho):
         for j in cities:
             pheromones[i][j] = (1 - rho) * pheromones[i][j] + delta_pheromones[i][j]
 
+def plot_tour(tour):
+    # Plot a tour
+    x = [distances[tour[i]][0] for i in range(N + 1)]
+    y = [distances[tour[i]][1] for i in range(N + 1)]
+    plt.plot(x, y, 'o-')
+    for i in range(N + 1):
+        plt.annotate(str(i + 1), (x[i], y[i]))
+    plt.show()
+
 # Conduct a sequence of simulations on the same map
 for M in M_values:
-    # Initialize pheromone levels
     pheromones = [[1 / (N * N) for _ in cities] for _ in cities]
-
-    # Run the ant algorithm
-    for step in range(100):
+    for step in range(50):
         tours = construct_solution(M, N, alpha, beta)
         update_pheromones(tours, M, N, rho)
-
-    # Find the best tour
     best_tour = min(tours,key=length)
     best_length = length(best_tour)
-
     print(f'M: {M}, rho: {rho}, alpha: {alpha}, beta: {beta}')
     print(f'Best tour: {best_tour}')
     print(f'Best length: {best_length}')
 
 for rho in rho_values:
-    # Initialize pheromone levels
     pheromones = [[1 / (N * N) for _ in cities] for _ in cities]
-
-    # Run the ant algorithm
-    for step in range(100):
+    for step in range(50):
         tours = construct_solution(M, N, alpha, beta)
         update_pheromones(tours, M, N, rho)
-
-    # Find the best tour
     best_tour = min(tours,key=length)
     best_length = length(best_tour)
-
     print(f'M: {M}, rho: {rho}, alpha: {alpha}, beta: {beta}')
     print(f'Best tour: {best_tour}')
     print(f'Best length: {best_length}')
-
-for rho in rho_values:
-    # Initialize pheromone levels
-    pheromones = [[1 / (N * N) for _ in cities] for _ in cities]
-
-    # Run the ant algorithm
-    for step in range(100):
-        tours = construct_solution(M, N, alpha, beta)
-        update_pheromones(tours, M, N, rho)
-
-    # Find the best tour
-    best_tour = min(tours,key=length)
-    best_length = length(best_tour)
-
-    print(f'M: {M}, rho: {rho}, alpha: {alpha}, beta: {beta}')
-    print(f'Best tour: {best_tour}')
-    print(f'Best length: {best_length}')
+    plot_tour(best_tour)
 
 for alpha, beta in zip(alpha_values, beta_values):
     # Initialize pheromone levels
     pheromones = [[1 / (N * N) for _ in cities] for _ in cities]
-
-    # Run the ant algorithm
-    for step in range(100):
+    for step in range(50):
         tours = construct_solution(M, N, alpha, beta)
         update_pheromones(tours, M, N, rho)
-
-    # Find the best tour
     best_tour = min(tours,key=length)
     best_length = length(best_tour)
-
     print(f'M: {M}, rho: {rho}, alpha: {alpha}, beta: {beta}')
     print(f'Best tour: {best_tour}')
     print(f'Best length: {best_length}')
+    plot_tour(best_tour)
